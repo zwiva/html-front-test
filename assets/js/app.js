a003e5e58d4d6af7ec0f520e5f2144fae9cd1ce8
Andrea Castro <ap.castrocastellon@gmail.com>
2021-05-13 20:43:16 -0400

    agrego jquery

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,28 @@
 console.log('aaaaaa');
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+
+$(function () {
+  $(".card-text").on("dblclick", function () {
+    $(this).css("color", "black");
+    $(this).css("font-weight", "bold");
+  });
+  $(".card-img-top").on("click", function () {
+    $(".card-body").toggle();
+  });
+});
+
+$(function (){
+  $("a").click(function(event){
+      if (this.hash !== "") {
+          event.preventDefault();
+          var ids = this.hash;
+          $("html, body").animate({scrollTop: $(ids).offset().top}, 800, function() {
+              window.location.hash = ids;
+          });
+      }
+  });
+});
